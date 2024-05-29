18dfd01b95e51fae25d2054db5b9d30d77a953e3
LoboProgramming <matheusloboo2001@gmail.com>
2024-05-29 18:45:03 -0400

    modificando models/admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email'
+    ordering = 'id',
+    list_filter = 'created_date',
+    search_fields = 'id','first_name','last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+    # list_editable = 'first_name', 'last_name'
+    list_display_links = 'id','first_name'
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
