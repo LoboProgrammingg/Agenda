8d6cdae1e56a74a5f5c0ec7850d497177d06cc6f
LoboProgramming <matheusloboo2001@gmail.com>
2024-05-30 18:03:36 -0400

    new changes

--- project/settings.py
+++ project/settings.py
@@ -131,3 +131,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
