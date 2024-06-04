0dc2e53e85da5a0abd4405a69af2e589b8dfc717
LoboProgramming <matheusloboo2001@gmail.com>
2024-06-04 14:49:04 -0400

    New V

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,14 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     first_name = forms.CharField(
         widget= forms.TextInput(
             attrs={
@@ -23,6 +31,7 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
 
     def clean(self):
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -9,7 +9,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
         context = {
             'form': form,
             'form_action': form_action,
@@ -41,7 +41,7 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
@@ -67,4 +67,21 @@ def update(request, contact_id):
         request,
         'contact/create.html',
         context
+    )
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
